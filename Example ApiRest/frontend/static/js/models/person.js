4c7da133e11d37f94ca25bfb8c5bae8cb6450803
Huitzoo <eckobik@gmail.com>
2019-03-27 11:37:41 -0600

    remove templates

--- Example ApiRest/frontend/static/js/models/person.js
+++
@@ -1,9 +0,0 @@
-class Person{
-    constructor(json){
-        this.id = json.id
-        this.username = json.username
-        this.phone = json.phone
-        this.address = json.addres
-    }
-    
-}
