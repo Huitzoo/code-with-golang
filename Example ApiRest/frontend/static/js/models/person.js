773ef4ef147e691dcd9f4be25771d1d2aadced91
Huitzoo <eckobik@gmail.com>
2019-03-27 11:38:07 -0600

    add service templates

---
+++ Example ApiRest/frontend/static/js/models/person.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(json){
+        this.id = json.id
+        this.username = json.username
+        this.phone = json.phone
+        this.address = json.addres
+    }
+    
+}
